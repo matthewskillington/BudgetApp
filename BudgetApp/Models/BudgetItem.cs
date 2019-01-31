88cb11459d02a370cc4921aa064560e84f8905d8
Matt <matthew.skillington@ideagenplc.com>
2019-01-31 14:44:12 +0000

    Adding list view functionality to information page

---
+++ BudgetApp/Models/BudgetItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace BudgetApp.Models
+{
+    public class BudgetItem : BindableObject
+    {
+        public BudgetItem(int id, string labelName)
+        {
+            Id = id;
+            LabelName = labelName;
+
+        }
+
+        public int Id { get; set; }
+
+        public string LabelName { get; set; }
+
+        public string Name { get; set; }
+
+        public int? Amount { get; set; }
+
+
+
+    }
+}
--- BudgetApp/ViewModels/InformationPageViewModel.cs
+++ BudgetApp/ViewModels/InformationPageViewModel.cs
@@ -1,13 +1,40 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using BudgetApp.Models;
+using Xamarin.Forms;
+
 namespace BudgetApp.ViewModels
 {
-    public class InformationPageViewModel
+    public class InformationPageViewModel : BindableObject
     {
         public InformationPageViewModel()
         {
             InfoPageLabel = "Information Page";
+            budgetItems = new ObservableCollection<BudgetItem>();
         }
 
         public string InfoPageLabel { get; private set; }
+
+        private ObservableCollection<BudgetItem> _budgetItems;
+
+        public ObservableCollection<BudgetItem> budgetItems {
+            get
+            {
+                return _budgetItems;
+            }
+            set
+            {
+                _budgetItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand => new Command(AddNewBudgetItem);
+
+        private void AddNewBudgetItem()
+        {
+            budgetItems.Add(new BudgetItem(6, "Item 2"));
+        }
     }
 }
